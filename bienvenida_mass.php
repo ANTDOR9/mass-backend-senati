2fcd55b795e73fa1f96aa552abc575570d4e9894
Anthony Dorly Huilahuaña Chata <1677406@senati.pe>
2026-05-23 14:48:43 -0500

    Actualización de ejercicios del Minimarket Mass

--- bienvenida_mass.php
+++ bienvenida_mass.php
@@ -12,22 +12,19 @@ $nombre_tienda = "BOREALIS";
 $nombre_propietario = "ANTHONY DORLY HUILAHUAÑA CHATA";
 $fecha_hoy = date("d/m/Y");
 $categorias = array("Abarrotes", "Lácteos y huevos", "Bebidas y gaseosas");
-$promocion_dia = "¡Lleva 2 panes y llévate 1 gratis! Válido hasta agotar stock.";
+$promocion_dia = "Lleva 2 panes y llévate 1 gratis! Válido hasta agotar stock.";
 ?>
 
+<center>
 <h1>Bienvenido a Mass — <?php echo $nombre_tienda; ?></h1>
-
 <p>Hoy es: <?php echo $fecha_hoy; ?></p>
-
 <p>Atendido por: <?php echo $nombre_propietario; ?></p>
-
 <ul>
-    <li><?php echo $categorias[0]; ?></li>
-    <li><?php echo $categorias[1]; ?></li>
-    <li><?php echo $categorias[2]; ?></li>
+<?php echo $categorias[0]; ?><br>
+<?php echo $categorias[1]; ?><br>
+<?php echo $categorias[2]; ?><br>
 </ul>
-
 <p><strong>Promoción del día: <?php echo $promocion_dia; ?></strong></p>
-
+ </center>
 </body>
 </html>
--- info.php
+++ info.php
@@ -1,4 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Minimarket Mass</h1>
 <?php
-echo "hola mundo!";
-
+$cliente = "María García";
+$puntos = 1250;
 ?>
+<p>Hola, <?php echo $cliente; ?> </p>
+<p>Tienes <?= $puntos ?> puntos acumulados.</p>
+</body>
+</html>
